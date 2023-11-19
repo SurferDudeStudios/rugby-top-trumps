ce3606a0138f066ac17495bef43f64f9c3ff6e75
Surfer Dude Studios <41343948+SurferDudeStudios@users.noreply.github.com>
2023-11-19 13:59:05 +0000

    Build: Default opening build

--- src/Components/DefaultOpening.js
+++ src/Components/DefaultOpening.js
@@ -1,5 +1,22 @@
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+
 const DefaultOpening = () => {
-  return <p>Default opening</p>;
+  return (
+    <article id="laws-of-the-game">
+      <section>
+        <Container>
+          <Row>
+            <Col>
+              <h2>What would you like yo do?</h2>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </article>
+  );
 };
 
 export default DefaultOpening;
